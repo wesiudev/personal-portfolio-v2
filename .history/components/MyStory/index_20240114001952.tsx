508a8397fcdc20d5bddfd26e19dc1a042763f226
wesselpawel <wesselpawel@wp.pl>
2024-01-14 05:16:35 +0100

    donut change

---
+++ .history/components/MyStory/index_20240114001952.tsx
@@ -0,0 +1,58 @@
+import Link from "next/link";
+import { FaEnvelope, FaPhone } from "react-icons/fa";
+
+export default function MyStory() {
+  return (
+    <div
+      id="about"
+      className="w-screen flex flex-col items-center text-center relative z-0 min-h-screen mt-[50vh]"
+    >
+      <h2 className="text-[#F7E11F] text-7xl text-center font-bold w-max mx-auto">
+        My Story
+      </h2>
+      <div className="text-[#F7E11F] text-lg lg:text-2xl space-y-12 w-[80vw] lg:w-[70vw] xl:w-[60vw] mt-16">
+        <p className="">
+          In 2015, I started studying computer science at a technical high
+          school. At first, programming didn't really interest me, and I
+          probably wouldn't have gotten into it if I passed final exams on the
+          first attempt.
+        </p>
+        <p className="">
+          While preparing for the exam retakes, I discovered a passion for
+          programming.{" "}
+        </p>
+        <p className="">
+          Wanting to learn more, I focused on PHP and created my first project,
+          which had payment system. Of course it was a big failure.
+        </p>
+        <p className="">
+          After passing the exams, I decided to explore coding further,
+          concentrating on a JavaScript library - React.
+        </p>
+        <p className="">
+          This led me to meet my{" "}
+          <Link title="Visit page" target="_blank" href="https://sebster.dev">
+            mentor
+          </Link>
+          , an experienced professional who not only reviewed my code but also
+          presented me with exciting challenges that helped me improve.
+        </p>
+        <p className="">
+          With new skills and a collection of projects, I made the intentional
+          choice to look for a job in my chosen field.
+        </p>
+      </div>
+      <div className="text-white font-light text-xl italic opacity-50 h-screen flex flex-col items-center justify-evenly">
+        wesiu.dev 2024
+        <div className="flex flex-row items-center w-full justify-between">
+          <Link title="Send me an email" href="mailto:wesiudev@gmail.com">
+            <FaEnvelope className="h-8 w-8 mt-4" />
+          </Link>
+          <Link title="Call me" href="tel:+48721417154">
+            <FaPhone className="h-8 w-8 mt-4" />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ .history/components/Navigation/index_20240114002602.tsx
@@ -0,0 +1,72 @@
+"use client";
+import * as Scroll from "react-scroll";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import Link from "next/link";
+
+export const NavRight: React.FC = () => {
+  const ScrollTo = Scroll.Link;
+  return (
+    <div className="text-gray-300 lg:flex hidden fixed right-12 top-12 flex-col-reverse lg:flex-row lg:space-x-3 z-[590] font-regular text-xl">
+      <ScrollTo
+        to="projects"
+        smooth={true}
+        duration={500}
+        delay={200}
+        data-aos="fade-down"
+        data-aos-delay={2600}
+        className="group h-max overflow-hidden cursor-pointer flex flex-row items-center"
+      >
+        <div className="relative group-hover:text-white duration-200 ">
+          <div className="absolute -left-[120%] group-hover:-left-0 duration-300 ease-in-out top-0 h-full w-full bg-yellow-400"></div>
+          <div className="z-[50] relative font-light bg-black bg-opacity-30 px-2">
+            Portfolio
+          </div>
+        </div>
+      </ScrollTo>
+      <ScrollTo
+        to="about"
+        smooth={true}
+        duration={500}
+        delay={200}
+        data-aos="fade-down"
+        data-aos-delay={2800}
+        className="group h-max overflow-hidden cursor-pointer flex flex-row items-center"
+      >
+        <div className="relative group-hover:text-white duration-200 ">
+          <div className="absolute -left-[120%] group-hover:-left-0 duration-300 ease-in-out top-0 h-full w-full bg-yellow-400"></div>
+          <div className="z-[50] relative font-light bg-black bg-opacity-30  px-2">
+            About
+          </div>
+        </div>
+      </ScrollTo>
+    </div>
+  );
+};
+
+export const NavLeft: React.FC = () => {
+  return (
+    <div
+      data-aos="fade-in"
+      data-aos-delay={2600}
+      className="fixed left-6 lg:left-12 bottom-12 flex flex-col items-center justify-center space-y-3 z-[550] group bg-black bg-opacity-50 p-1.5"
+    >
+      <Link
+        target="_blank"
+        title="Przejdź do Github.com"
+        href="https://github.com/wesiudev"
+        className="opacity-70 hover:opacity-100 duration-200 text-gray-400"
+      >
+        <FaGithub className="h-8 w-8" />
+      </Link>
+      <Link
+        target="_blank"
+        title="Przejdź do Linkedin.com"
+        href="https://linkedin.com/in/wesiudev"
+        className="opacity-70 hover:opacity-100 duration-200 text-gray-400"
+      >
+        <FaLinkedin className="h-8 w-8" />
+      </Link>
+      <div className="w-px h-24 bg-zinc-700 hidden lg:block opacity-70 group-hover:opacity-100 duration-200"></div>
+    </div>
+  );
+};
