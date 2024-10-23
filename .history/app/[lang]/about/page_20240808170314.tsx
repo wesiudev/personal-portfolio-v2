ad5cc37609470b25991a01574b793e062219d629
wesselpawel <wesselpawel@wp.pl>
2024-10-23 22:46:52 +0200

    metadata

--- .history/app/[lang]/about/page_20240808170314.tsx
+++ .history/app/[lang]/about/page_20240808170314.tsx
@@ -2,7 +2,7 @@ import { getDictionary } from "@/get-dictionary";
 import { Locale } from "@/i18n-config";
 import Content from "./Content";
 
-export default async function Page({ params }: { params: { lang: any } }) {
+export default async function Page({ params }: { params: { lang: Locale } }) {
   const dictionary = await getDictionary(params.lang);
   return (
     <div>
